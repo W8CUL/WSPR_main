535e1d96c523e51b07e0133ed5738edadc146db9
Hasith Perera <12579141+hasithperera@users.noreply.github.com>
2025-02-14 21:30:35 -0500

    GPS synced FT8 output

--- WSPR_encode.ino
+++ WSPR_encode.ino
@@ -43,39 +43,46 @@
 #include "Wire.h"
 
 // Mode defines
-#define JT9_TONE_SPACING        174          // ~1.74 Hz
-#define JT65_TONE_SPACING       269          // ~2.69 Hz
-#define JT4_TONE_SPACING        437          // ~4.37 Hz
-#define WSPR_TONE_SPACING       146          // ~1.46 Hz
-#define FSQ_TONE_SPACING        879          // ~8.79 Hz
-#define FT8_TONE_SPACING        625          // ~6.25 Hz
+#define JT9_TONE_SPACING 174   // ~1.74 Hz
+#define JT65_TONE_SPACING 269  // ~2.69 Hz
+#define JT4_TONE_SPACING 437   // ~4.37 Hz
+#define WSPR_TONE_SPACING 146  // ~1.46 Hz
+#define FSQ_TONE_SPACING 879   // ~8.79 Hz
+#define FT8_TONE_SPACING 625   // ~6.25 Hz
 
-#define JT9_DELAY               576          // Delay value for JT9-1
-#define JT65_DELAY              371          // Delay in ms for JT65A
-#define JT4_DELAY               229          // Delay value for JT4A
-#define WSPR_DELAY              683          // Delay value for WSPR
-#define FSQ_2_DELAY             500          // Delay value for 2 baud FSQ
-#define FSQ_3_DELAY             333          // Delay value for 3 baud FSQ
-#define FSQ_4_5_DELAY           222          // Delay value for 4.5 baud FSQ
-#define FSQ_6_DELAY             167          // Delay value for 6 baud FSQ
-#define FT8_DELAY               159          // Delay value for FT8
+#define JT9_DELAY 576      // Delay value for JT9-1
+#define JT65_DELAY 371     // Delay in ms for JT65A
+#define JT4_DELAY 229      // Delay value for JT4A
+#define WSPR_DELAY 683     // Delay value for WSPR
+#define FSQ_2_DELAY 500    // Delay value for 2 baud FSQ
+#define FSQ_3_DELAY 333    // Delay value for 3 baud FSQ
+#define FSQ_4_5_DELAY 222  // Delay value for 4.5 baud FSQ
+#define FSQ_6_DELAY 167    // Delay value for 6 baud FSQ
+#define FT8_DELAY 159      // Delay value for FT8
 
-#define JT9_DEFAULT_FREQ        14078700UL
-#define JT65_DEFAULT_FREQ       14078300UL
-#define JT4_DEFAULT_FREQ        14078500UL
-#define WSPR_DEFAULT_FREQ       14097200UL
-#define FSQ_DEFAULT_FREQ        7105350UL     // Base freq is 1350 Hz higher than dial freq in USB
-#define FT8_DEFAULT_FREQ        14075000UL
+#define JT9_DEFAULT_FREQ 14078700UL
+#define JT65_DEFAULT_FREQ 14078300UL
+#define JT4_DEFAULT_FREQ 14078500UL
+#define WSPR_DEFAULT_FREQ 14097200UL
+#define FSQ_DEFAULT_FREQ 7105350UL  // Base freq is 1350 Hz higher than dial freq in USB
+#define FT8_DEFAULT_FREQ 14075000UL
 
-#define DEFAULT_MODE            MODE_WSPR
+#define DEFAULT_MODE MODE_WSPR
 
 // Hardware defines
-#define BUTTON                  12
-#define LED_PIN                 13
+#define BUTTON 12
+#define LED_PIN 13
 
 // Enumerations
-enum mode {MODE_JT9, MODE_JT65, MODE_JT4, MODE_WSPR, MODE_FSQ_2, MODE_FSQ_3,
-  MODE_FSQ_4_5, MODE_FSQ_6, MODE_FT8};
+enum mode { MODE_JT9,
+            MODE_JT65,
+            MODE_JT4,
+            MODE_WSPR,
+            MODE_FSQ_2,
+            MODE_FSQ_3,
+            MODE_FSQ_4_5,
+            MODE_FSQ_6,
+            MODE_FT8 };
 
 // Class instantiation
 Si5351 si5351;
@@ -93,7 +100,7 @@ uint8_t symbol_count;
 uint16_t tone_delay, tone_spacing;
 
 // Loop through the string, transmitting one character at a time.
-void encode(){
+void encode() {
   uint8_t i;
 
   // Reset the tone to the base frequency and turn on the output
@@ -101,58 +108,34 @@ void encode(){
   digitalWrite(LED_PIN, HIGH);
 
   // Now transmit the channel symbols
-  if(cur_mode == MODE_FSQ_2 || cur_mode == MODE_FSQ_3 || cur_mode == MODE_FSQ_4_5 || cur_mode == MODE_FSQ_6)
-  {
+  if (cur_mode == MODE_FSQ_2 || cur_mode == MODE_FSQ_3 || cur_mode == MODE_FSQ_4_5 || cur_mode == MODE_FSQ_6) {
     uint8_t j = 0;
 
-    while(tx_buffer[j++] != 0xff);
+    while (tx_buffer[j++] != 0xff)
+      ;
 
     symbol_count = j - 1;
   }
 
-  for(i = 0; i < symbol_count; i++)
-  {
-      si5351.set_freq((freq * 100) + (tx_buffer[i] * tone_spacing), SI5351_CLK0);
-      delay(tone_delay);
+  for (i = 0; i < symbol_count; i++) {
+    si5351.set_freq((freq * 100) + (tx_buffer[i] * tone_spacing), SI5351_CLK0);
+    delay(tone_delay);
   }
-
   // Turn off the output
   si5351.output_enable(SI5351_CLK0, 0);
   digitalWrite(LED_PIN, LOW);
 }
 
-void set_tx_buffer(){
+void set_tx_buffer() {
   // Clear out the transmit buffer
   memset(tx_buffer, 0, 255);
+  //jtencode.wspr_encode(call, loc, dbm, tx_buffer);
 
-  // Set the proper frequency and timer CTC depending on mode
-  switch(cur_mode)
-  {
-  case MODE_JT9:
-    jtencode.jt9_encode(message, tx_buffer);
-    break;
-  case MODE_JT65:
-    jtencode.jt65_encode(message, tx_buffer);
-    break;
-  case MODE_JT4:
-    jtencode.jt4_encode(message, tx_buffer);
-    break;
-  case MODE_WSPR:
-    jtencode.wspr_encode(call, loc, dbm, tx_buffer);
-    break;
-  case MODE_FT8:
-    jtencode.ft8_encode(message, tx_buffer);
-    break;
-  case MODE_FSQ_2:
-  case MODE_FSQ_3:
-  case MODE_FSQ_4_5:
-  case MODE_FSQ_6:
-    jtencode.fsq_dir_encode(call, "n0call", ' ', "hello world", tx_buffer);
-    break;
-  }
+  jtencode.ft8_encode(message, tx_buffer);
 }
 
-void setup1(){
+
+void setup_WSPR() {
   // Initialize the Si5351
   // Change the 2nd parameter in init if using a ref osc other
   // than 25 MHz
@@ -164,7 +147,6 @@ void setup1(){
 
   // Use a button connected to pin 12 as a transmit trigger
   pinMode(BUTTON, INPUT_PULLUP);
-
   // Set the mode to use
   //cur_mode = MODE_JT65;
   cur_mode = MODE_FT8;
@@ -172,78 +154,12 @@ void setup1(){
 
   // Set the proper frequency, tone spacing, symbol count, and
   // tone delay depending on mode
-  switch(cur_mode)
-  {
-  case MODE_JT9:
-    freq = JT9_DEFAULT_FREQ;
-    symbol_count = JT9_SYMBOL_COUNT; // From the library defines
-    tone_spacing = JT9_TONE_SPACING;
-    tone_delay = JT9_DELAY;
-    break;
-  case MODE_JT65:
-    freq = JT65_DEFAULT_FREQ;
-    symbol_count = JT65_SYMBOL_COUNT; // From the library defines
-    tone_spacing = JT65_TONE_SPACING;
-    tone_delay = JT65_DELAY;
-    break;
-  case MODE_JT4:
-    freq = JT4_DEFAULT_FREQ;
-    symbol_count = JT4_SYMBOL_COUNT; // From the library defines
-    tone_spacing = JT4_TONE_SPACING;
-    tone_delay = JT4_DELAY;
-    break;
-  case MODE_WSPR:
-    freq = WSPR_DEFAULT_FREQ;
-    symbol_count = WSPR_SYMBOL_COUNT; // From the library defines
-    tone_spacing = WSPR_TONE_SPACING;
-    tone_delay = WSPR_DELAY;
-    break;
-  case MODE_FT8:
-    freq = FT8_DEFAULT_FREQ;
-    symbol_count = FT8_SYMBOL_COUNT; // From the library defines
-    tone_spacing = FT8_TONE_SPACING;
-    tone_delay = FT8_DELAY;
-    break;
-  case MODE_FSQ_2:
-    freq = FSQ_DEFAULT_FREQ;
-    tone_spacing = FSQ_TONE_SPACING;
-    tone_delay = FSQ_2_DELAY;
-    break;
-  case MODE_FSQ_3:
-    freq = FSQ_DEFAULT_FREQ;
-    tone_spacing = FSQ_TONE_SPACING;
-    tone_delay = FSQ_3_DELAY;
-    break;
-  case MODE_FSQ_4_5:
-    freq = FSQ_DEFAULT_FREQ;
-    tone_spacing = FSQ_TONE_SPACING;
-    tone_delay = FSQ_4_5_DELAY;
-    break;
-  case MODE_FSQ_6:
-    freq = FSQ_DEFAULT_FREQ;
-    tone_spacing = FSQ_TONE_SPACING;
-    tone_delay = FSQ_6_DELAY;
-    break;
-  }
-
+  freq = FT8_DEFAULT_FREQ;
+  symbol_count = FT8_SYMBOL_COUNT;  // From the library defines
+  tone_spacing = FT8_TONE_SPACING;
+  tone_delay = FT8_DELAY;
   // Set CLK0 output
-  si5351.drive_strength(SI5351_CLK0, SI5351_DRIVE_8MA); // Set for max power if desired
-  si5351.output_enable(SI5351_CLK0, 0); // Disable the clock initially
-
-  // Encode the message in the transmit buffer
-  // This is RAM intensive and should be done separately from other subroutines
+  si5351.drive_strength(SI5351_CLK0, SI5351_DRIVE_8MA);  // Set for max power if desired
+  si5351.output_enable(SI5351_CLK0, 0);                  // Disable the clock initially
   set_tx_buffer();
-}
-
-void loop1(){
-  // Debounce the button and trigger TX on push
-  if(digitalRead(BUTTON) == LOW)
-  {
-    delay(50);   // delay to debounce
-    if (digitalRead(BUTTON) == LOW)
-    {
-      encode();
-      delay(50); //delay to avoid extra triggers
-    }
-  }
 }
--- WSPR_main.ino
+++ WSPR_main.ino
@@ -9,31 +9,178 @@
 #include "Wire.h"
 
 SoftwareSerial gps(3, 2);  // RX, TX
+char test_data[100];
+
+
+char Lat[] = "xxxx.xxxxxx";
+char Lon[] = "xxxxx.xxxxxxxx";
+char time_str[12];
+char tmp_str[15];
+int msg_valid = 0;
+float lon,lat,utc;
+
+
+#define FT8 1
+
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println("Testing");
+  Serial.println(F("Testing"));
 
   gps.begin(9600);
+  setup_WSPR();
 }
 
-#define sim 1
+#define sim 0
+//#define debug_low 1 - //unconnent only when needdee
+
 
 void loop() {
   // put your main code here, to run repeatedly:
   while (gps.available() > 0) {
     String gps_raw = gps.readStringUntil('\n');
-    Serial.println(gps_raw);
+    #ifdef debug_low
+      Serial.println(gps_raw);
+    #endif
     //sim packet  $GPGLL,3938.76279,N,07958.40013,W,175359.00,A,A*7E
     if (gps_raw.substring(0, 6) == "$GPGLL") {
-      if (sim == 1) {
-        gps_raw = "$GPGLL,3938.76279,N,07958.40013,W,175359.00,A,A*7E";
-        //v4.2 test data
-      } else {
-        //print raw GPS data
-        Serial.println(gps_raw);
-      }
+
+      #ifdef debug_low
+        if (sim == 1) {
+          gps_raw = "$GPGLL,3938.76279,N,07958.40013,W,175359.00,A,A*7E";
+          //v4.2 test data
+          //Serial.println(gps_raw);
+        } else {
+          //print raw GPS data
+          //Serial.println(gps_raw);
+        }
+      #endif
+      gps_raw.toCharArray(test_data, 100);
+      char *p = strtok(test_data, ",");
+      update_GPS(p);
     }
   }
+}
+
+
+void update_GPS(char *p) {
+
+  //data is sent with 2 decimal places
+  // reformat the data to be used by the APRS library
+  //"$GPGGA,191757.00,3938.28486,N,07957.13511,W,1,03,2.71,274.5,M,-33.9,M,,*6F";
+  // v3
+  // $GNGGA,165006.000,2241.9107,N,12017.2383,E,1,14,0.79,22.6,M,18.5,M,,*42
+
+  // Documentation: https://openrtk.readthedocs.io/en/latest/communication_port/nmea.html
+  //$GNGGA<0>,000520.095<1>,<2>,<3>,<4>,<5>,0<6>,0<7>,<8>,<9>,M<10>,<11>,M<12>,<13>,*5D<14>
+
+  //"$GPGLL,3938.76279,N,07958.40013,W,175359.00,A,A*7E";
+  //"$GPGLL<0>,3938.76279<1>,N<2>,07958.40013<3>,W<4>,175359.00<5>,A,A*7E";
+
+  //char *p = strtok(test_data, ",");  //code - <0>
+
+
+  p = strtok(NULL, ",");             //lat - <1>
+  //sprintf(Lat, "%s", p);
+  lat = atof(p)/100.00;
+
+  
+  // bug fix in v2
+  if (Lat[4] == '.') {
+    Lat[7] = '\0';
+  } else {
+    Lat[8] = '\0';
+  }
+  
+
+  
+  p = strtok(NULL, ",");  // lat_char - <2>
+  //sprintf(Lat, "%s%s\0", Lat, p);
+
+  //p = strtok(NULL, ",");             //lng
+  //p = strtok(NULL, ",");             //dir
+
+  p = strtok(NULL, ",");  //lng - <3>
+  lon = atof(p);
+  //sprintf(Lon, "%s", p);
+
+  if (Lon[4] == '.') {
+    Lon[7] = '\0';
+  } else {
+    Lon[8] = '\0';
+  }
+
+  p = strtok(NULL, ",");  //dir - <4>
+  sprintf(Lon, "%s%s\0", Lon, p);
+
+  p = strtok(NULL, ",");  //state - <5>
+  utc = atof(p);
+
+  //Serial.print("-");
+  //Serial.println(p);
+
+
+  msg_valid = 1;
+
+ 
+  //Serial.println(utc);
+  //Serial.print(long(utc));
+
+  /*
+  if(long(utc)%200==0){
+
+    Serial.print(lat);
+    Serial.print(",");
+    Serial.println(lon);
+    Serial.println(utc);
+
+    Serial.println("WSPR - 20m");
+    encode();
+    delay(50); //delay to avoid extra triggers
+    Serial.println("End of WSPR");
+  }else{
+    Serial.println(long(utc));
+  }
+  */
+
+  
+  int sec = long(utc)%100;
+  int min = long(utc)%10000 - sec;
+
+  Serial.print(min);
+  Serial.print(",");
+  Serial.println(sec);
+
+  #ifdef FT8
+  if(sec==30){
+
+ 
+  
+    Serial.print(lat);
+    Serial.print(",");
+    Serial.println(lon);
+
+    Serial.println("FT8 - 20m");
+    gps.stopListening();
+    encode();
+    delay(50); //delay to avoid extra triggers
+    gps.listen();
+    Serial.println("End FT8");
+  }else{
+    Serial.println(long(utc));
+
+  }
+  
+  #endif
+
+  #ifdef WSPR
+  // WSPR
+
+
+  #endif
+
+
+
 }
